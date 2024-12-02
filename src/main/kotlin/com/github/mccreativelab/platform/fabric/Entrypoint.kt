e910e8eab096d9f85c451a1fab54a0c88172ae4c
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-02 20:29:28 +0100

    added logger

--- src/main/kotlin/com/github/mccreativelab/platform/fabric/Entrypoint.kt
+++ src/main/kotlin/com/github/mccreativelab/platform/fabric/Entrypoint.kt
@@ -1,5 +1,12 @@
 package com.github.mccreativelab.platform.fabric
 
+import java.util.logging.Logger
+
+/**
+ * The logger for the mod.
+ */
+val logger = Logger.getLogger("mcc-platform-fabric")
+
 /**
  * The main entrypoint for the mod.
  */
