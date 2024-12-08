ddcde0c8b621510b69d3468463be427cd6d68f46
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-08 12:02:13 +0100

    start FabricPlatform from scratch

--- src/main/kotlin/com/github/mccreativelab/platform/fabric/FabricPlatform.kt
+++ src/main/kotlin/com/github/mccreativelab/platform/fabric/FabricPlatform.kt
@@ -1,18 +1,5 @@
 package com.github.mccreativelab.platform.fabric
 
 import de.verdox.mccreativelab.impl.vanilla.platform.NMSPlatform
-import de.verdox.mccreativelab.wrapper.entity.MCCPlayer
-import net.silkmc.silk.core.Silk
-import java.util.*
 
-class FabricPlatform: NMSPlatform() {
-
-    // TODO: remove when implemented in the super class
-    override fun getOnlinePlayer(uuid: UUID?): MCCPlayer? {
-        return if (uuid == null) null
-        else conversionService.wrap(
-            Silk.server?.playerList?.getPlayer(uuid),
-            MCCPlayer::class.java
-        )
-    }
-}
+class FabricPlatform: NMSPlatform()
