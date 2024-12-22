3cad2fd57be3c10219ee50f8b28ad961284321d2
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-22 17:28:54 +0100

    add MCCVehicleDamageEvent

---
+++ src/main/java/com/github/mccreativelab/platform/fabric/mixins/MixinVehicleEntity.java
@@ -0,0 +1,31 @@
+package com.github.mccreativelab.platform.fabric.mixins;
+
+import com.google.common.reflect.TypeToken;
+import de.verdox.mccreativelab.wrapper.entity.MCCEntity;
+import de.verdox.mccreativelab.wrapper.event.vehicle.MCCVehicleDamageEvent;
+import de.verdox.mccreativelab.wrapper.platform.MCCPlatform;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.vehicle.VehicleEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(VehicleEntity.class)
+public class MixinVehicleEntity {
+
+    @Inject(at = @At("HEAD"), method = "hurt")
+    public void injectHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        // MCCEntity vehicle, MCCEntity attacker, double damage, boolean cancelled
+        MCCVehicleDamageEvent event = new MCCVehicleDamageEvent(
+                MCCPlatform.getInstance().getConversionService().wrap((VehicleEntity) (Object) this, TypeToken.of(MCCEntity.class)),
+                MCCPlatform.getInstance().getConversionService().wrap((VehicleEntity) (Object) source.getEntity(), TypeToken.of(MCCEntity.class)), // TODO: check if this is correct - because there is also getDirectEntity()
+                amount,
+                cir.isCancelled()
+        );
+
+        if (event.callEvent()) {
+            cir.cancel(); // TODO: check if this is correct
+        }
+    }
+}
