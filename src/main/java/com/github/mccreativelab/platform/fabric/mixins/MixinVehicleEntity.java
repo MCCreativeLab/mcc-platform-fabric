d07b43c8d2da867ad607385306f9084d4f7f9ab0
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2025-01-07 10:40:52 +0100

    remove redundant comment

--- src/main/java/com/github/mccreativelab/platform/fabric/mixins/MixinVehicleEntity.java
+++ src/main/java/com/github/mccreativelab/platform/fabric/mixins/MixinVehicleEntity.java
@@ -29,7 +29,6 @@ public class MixinVehicleEntity {
      */
     @Inject(at = @At("HEAD"), method = "hurt")
     public void injectHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        // MCCEntity vehicle, MCCEntity attacker, double damage, boolean cancelled
         MCCVehicleDamageEvent event = new MCCVehicleDamageEvent(
                 MCCPlatform.getInstance().getConversionService().wrap((VehicleEntity) (Object) this, TypeToken.of(MCCEntity.class)),
                 MCCPlatform.getInstance().getConversionService().wrap((VehicleEntity) source.getEntity(), TypeToken.of(MCCEntity.class)), // TODO: check if this is correct - because there is also getDirectEntity()
