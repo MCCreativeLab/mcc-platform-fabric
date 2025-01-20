0646d9df83c49b2aa4f755a6b5222b2ec87fe73e
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2025-01-20 11:03:40 +0100

    add javadocs for ComponentConverter

--- src/main/kotlin/com/github/mccreativelab/platform/fabric/converter/ComponentConverter.kt
+++ src/main/kotlin/com/github/mccreativelab/platform/fabric/converter/ComponentConverter.kt
@@ -9,21 +9,43 @@ import net.kyori.adventure.text.Component as AdventureComponent
 import net.minecraft.network.chat.Component as NativeComonent
 
 // TODO: Test this converter
+/**
+ * A converter for components.
+ */
 class ComponentConverter: MCCConverter<NativeComonent, AdventureComponent> {
+
+    /**
+     * Wraps a native component.
+     * @param nativeType The native component.
+     * @return The wrapped component.
+     */
     override fun wrap(nativeType: Component?): MCCConverter.ConversionResult<net.kyori.adventure.text.Component> {
         val json = Serializer.toJson(nativeType ?: return notDone(null), Silk.server!!.registryAccess())
         return done(GsonComponentSerializer.gson().deserialize(json))
     }
 
+    /**
+     * Unwraps a platform implementation component.
+     * @param platformImplType The platform implementation component.
+     * @return The unwrapped component.
+     */
     override fun unwrap(platformImplType: net.kyori.adventure.text.Component?): MCCConverter.ConversionResult<Component> {
         val json = GsonComponentSerializer.gson().serialize(platformImplType ?: return notDone(null))
         return done(Serializer.fromJson(json, Silk.server!!.registryAccess()))
     }
 
+    /**
+     * Gets the class of the platform implementation.
+     * @return The class of the platform implementation.
+     */
     override fun apiImplementationClass(): Class<net.kyori.adventure.text.Component> {
         return AdventureComponent::class.java
     }
 
+    /**
+     * Gets the class of the native Minecraft type.
+     * @return The class of the native Minecraft type.
+     */
     override fun nativeMinecraftType(): Class<Component> {
         return NativeComonent::class.java
     }
