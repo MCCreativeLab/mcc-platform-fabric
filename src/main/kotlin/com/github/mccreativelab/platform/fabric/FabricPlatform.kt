4038eada6dde61f5789628421d67a7e69cdd0bac
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2025-01-26 17:19:25 +0100

    remove redundant javadocs

--- src/main/kotlin/com/github/mccreativelab/platform/fabric/FabricPlatform.kt
+++ src/main/kotlin/com/github/mccreativelab/platform/fabric/FabricPlatform.kt
@@ -9,31 +9,18 @@ import net.kyori.adventure.text.Component
 import net.silkmc.silk.core.Silk
 import java.util.*
 
-/**
- * The Fabric platform.
- */
 class FabricPlatform : NMSPlatform() {
 
-    /**
-     * Initializes the platform.
-     */
     override fun init() {
         super.init()
         conversionService.registerConverterForNewImplType(Component::class.java, ComponentConverter())
         LOGGER.info("Fabric platform has been initialized.")
     }
 
-    /**
-     * Shuts down the platform.
-     */
     override fun shutdown() {
         Silk.serverOrThrow.stopServer()
     }
 
-    /**
-     * Gets the worlds on the server.
-     * @return The worlds on the server.
-     */
     override fun getWorlds(): List<MCCWorld> {
         val worlds: MutableList<MCCWorld> = LinkedList()
         Silk.serverOrThrow.allLevels.forEach { serverLevel ->
@@ -42,19 +29,10 @@ class FabricPlatform : NMSPlatform() {
         return worlds
     }
 
-    /**
-     * Gets the world with the given name.
-     * @param name The name of the world.
-     * @return The world with the given name.
-     */
     override fun getOnlinePlayer(uuid: UUID): MCCPlayer? {
         return conversionService.wrap(Silk.serverOrThrow.playerList.getPlayer(uuid))
     }
 
-    /**
-     * Gets the online players.
-     * @return The online players.
-     */
     override fun getOnlinePlayers(): List<MCCPlayer> {
         return Silk.serverOrThrow.playerList.players.map { serverPlayer ->
             getConversionService().wrap(
